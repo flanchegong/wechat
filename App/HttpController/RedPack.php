7dca60d98d21e564496860b16f711fac8e267a0a
flanchegong <flanchegong@163.com>
2018-09-19 12:07:04 +0800

    test

--- App/HttpController/RedPack.php
+++ App/HttpController/RedPack.php
@@ -6,6 +6,7 @@ namespace App\HttpController;
  * Date: 2018/9/13
  * Time: 15:18
  */
+use App\Utility\Loader;
 use EasySwoole\Core\Http\Request;
 use EasySwoole\Core\Http\Response;
 use EasyWeChat\Factory;
@@ -131,6 +132,20 @@ class RedPack extends Base
     public function transfers()
     {
         include_once "App\Utility\Loader.php";
+        # 配置参数
+        $config = array(
+            'token'          => '',
+            'appid'          => '',
+            'appsecret'      => '',
+            'encodingaeskey' => '',
+        );
+        # 加载对应操作接口
+        $wechat = &\Wechat\Loader::get('User', $config);
+        $userlist = $wechat->getUserList();
+
+        var_dump($userlist);
+        var_dump($wechat->errMsg);
+        var_dump($wechat->errCode);
         $openid = $this->request()->getRequestParam('openid');
         $amount = 100;//$this->request()->getRequestParam('amount');
         $bill_no = md5('flanche'.sha1('ideamake'.microtime()));
